de4e9979e840008d059feab655ae9c0a50392681
Joshua Sharkey <josharkey6@gmail.com>
2020-02-07 10:03:45 +0800

    draw keypoints and limb on imgs

--- imagetest.py
+++ imagetest.py
@@ -4,71 +4,54 @@ import os
 import tensorflow as tf
 import numpy as np
 import json
-parts = {
-	0: 'NOSE',
-	1: 'LEFT_EYE',
-  	2: 'RIGHT_EYE',
-  	3: 'LEFT_EAR',
-  	4: 'RIGHT_EAR',
-  	5: 'LEFT_SHOULDER',
-  	6: 'RIGHT_SHOULDER',
-  	7: 'LEFT_ELBOW',
-  	8: 'RIGHT_ELBOW',
-  	9: 'LEFT_WRIST',
-  	10: 'RIGHT_WRIST',
-  	11: 'LEFT_HIP',
-  	12: 'RIGHT_HIP',
-  	13: 'LEFT_KNEE',
-  	14: 'RIGHT_KNEE',
-  	15: 'LEFT_ANKLE',
-  	16: 'RIGHT_ANKLE'
-}
+from person import Person
 
-img = cv.imread('photos\standing\\3.jpg')
-print(img.shape)
-# img = tf.reshape(img, [1,257,257,3])
-img = tf.reshape(tf.image.resize(img, [257,257]), [1,257,257,3])
+def save_model_details(m):
+	with open('model_details.json', 'w') as outfile:
+		info = dict(list(enumerate(m.get_tensor_details())))
+		s = json.dumps(str(info))
+		outfile.write(s)
+def draw(person, img):
+	radius = 1
+	color = (0,0,255) #BGR
+	thickness = 3
+	for p in person.get_coords():
+		cv.circle(img,p,radius, color, thickness)
+	for p1, p2 in person.get_limbs():
+		cv.line(img, p1, p2, color, thickness)
+	return img
+
+
 
 model = tf.lite.Interpreter('models\posenet_mobilenet_v1_100_257x257_multi_kpt_stripped.tflite')
 model.allocate_tensors()
 input_details = model.get_input_details()
 output_details = model.get_output_details()
-
-with open('model_details.json', 'w') as outfile:
-	 info = dict(list(enumerate(model.get_tensor_details())))
-	 s = json.dumps(str(info))
-	 outfile.write(s)
-
-exit()
 floating_model = input_details[0]['dtype'] == np.float32
 
-if floating_model:
-	img = (np.float32(img) - 127.5) / 127.5
+for file in os.scandir('photos/standing'):
+	img = cv.imread(file.path)
+	print(file.path, "original shape: " , img.shape)
+	img2 = cv.resize(img, (257,257), interpolation = cv.INTER_LINEAR)
+	img = tf.reshape(tf.image.resize(img, [257,257]), [1,257,257,3])
+	if floating_model:
+		img = (np.float32(img) - 127.5) / 127.5
+	# print('input shape: {}, img shape: {}'.format(input_details[0]['shape'], img.shape))
+	model.set_tensor(input_details[0]['index'], img)
+	model.invoke()
 
 
-print('input shape: {}, img shape: {}'.format(input_details[0]['shape'], img.shape))
-# print(output_details)
-# input_data = decode_img(img)
+	output_data =  np.squeeze(model.get_tensor(output_details[0]['index']))# o()
+	offset_data = np.squeeze(model.get_tensor(output_details[1]['index']))
+	p = Person(output_data, offset_data)
 
-model.set_tensor(input_details[0]['index'], img)
-model.invoke()
-
-
-output_data =  model.get_tensor(output_details[0]['index'])# o()
-offset_data = model.get_tensor(output_details[1]['index'])
-results = np.squeeze(output_data)
-offsets_results = np.squeeze(offset_data)
-print("output shape: {}".format(output_data.shape))
-np.savez('sample3.npz', results, offsets_results)
+	img = draw(p, img2)
+	cv.imshow('coords', img2)
+	cv.waitKey(0)
+# print(p.to_string())
+# results = np.squeeze(output_data)
+# offsets_results = np.squeeze(offset_data)
+# print("output shape: {}".format(output_data.shape))
+# np.savez('sample3.npz', results, offsets_results)
 
 
-
-
-
-# top_k = results.argsort()[-5:][::-1]
-# print(top_k)
-# for i in top_k:
-# 	if floating_model:
-# 		print('{:08.6f}: {}'.format(float(results[i]), parts.get(i)))
-# 	else:
-# 		print('{:08.6f}: {}'.format(float(results[i] / 255.0), parts.get(i)))
--- person.py
+++ person.py
@@ -2,7 +2,7 @@ import numpy as np
 import math
 import cv2 as cv
 
-parts = {
+PARTS = {
 	0: 'NOSE',
 	1: 'LEFT_EYE',
   	2: 'RIGHT_EYE',
@@ -46,6 +46,26 @@ class Person():
         return keypoints
     def infer_pose(self, coords):
         return "Unknown"
+    def get_coords(self):
+        return [kp.point() for kp in self.keypoints]
+    def get_limbs(self):
+        pairs = [
+            (5,6),
+            (5,7), 
+            (7,9), 
+            (5,11), 
+            (11,13), 
+            (13,15),
+            (6,8),
+            (8,10),
+            (6,12),
+            (12,14),
+            (14,16),
+            (11,12)
+            ]
+        limbs = [(self.keypoints[i].point(), self.keypoints[j].point()) for i, j in pairs]
+        return limbs
+
     def to_string(self):
         return "\n".join([a.to_string() for a in self.keypoints])
 
@@ -56,15 +76,17 @@ class KeyPoint():
         self.x = x
         self.y = y
         self.index = index
-        self.body_part = parts.get(index)
+        self.body_part = PARTS.get(index)
         self.confidence = v
+    def point(self):
+        return int(self.y), int(self.x)
     def to_string(self):
         return 'part: {} location: {} confidence: {}'.format(self.body_part, (self.x, self.y), self.confidence)
 
 
 
-npz = np.load('sample3.npz')
-data = npz['arr_0']
-offsets = npz['arr_1']
-person = Person(data, offsets)
-print(person.to_string())
+# npz = np.load('sample3.npz')
+# data = npz['arr_0']
+# offsets = npz['arr_1']
+# person = Person(data, offsets)
+# print(person.to_string())
